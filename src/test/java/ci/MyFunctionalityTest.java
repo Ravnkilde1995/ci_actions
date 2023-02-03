9091eca37fcbd66a92f8a15911d58141f55e465c
Oliver Ravnkilde <ravnkilde1995@gmail.com>
2023-02-03 12:35:24 +0100

    connected db and made test setup for db

--- src/test/java/ci/MyFunctionalityTest.java
+++ src/test/java/ci/MyFunctionalityTest.java
@@ -6,11 +6,18 @@
  */
 package ci;
 
+import Persistence.DBconnector;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -31,8 +38,37 @@ public class MyFunctionalityTest {
     }
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        System.out.println("TESTINNNNGGGG");
+        Connection con = null;
+        try {
+            con = DBconnector.connection();
+            String createTable = "CREATE TABLE IF NOT EXISTS `startcode_test`.`usertable` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `fname` VARCHAR(45) NULL,\n" +
+                    "  `lname` VARCHAR(45) NULL,\n" +
+                    "  `pw` VARCHAR(45) NULL,\n" +
+                    "  `phone` VARCHAR(45) NULL,\n" +
+                    "  `address` VARCHAR(45) NULL,\n" +
+                    "  PRIMARY KEY (`id`));";
+            con.prepareStatement(createTable).executeUpdate();
+            con.prepareStatement("DELETE FROM `startcode_test`.`usertable`").executeUpdate();
+
+            String SQL = "INSERT INTO startcode_test.usertable (fname, lname, pw, phone, address) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "Hans");
+            ps.setString(2, "Hansen");
+            ps.setString(3, "Hemmelig123");
+            ps.setString(4, "40404040");
+            ps.setString(5,"Rolighedsvej 3");
+            ps.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @AfterEach
     public void tearDown() {
